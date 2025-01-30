5eab193cba041166b3f608643056d3ce4454463b
GALENDEZ, AQUILA F. <80321458+Animq-lang@users.noreply.github.com>
2025-01-31 00:31:34 +0800

    Update prime.controller.ts

--- assignments/src/prime/prime.controller.ts
+++ assignments/src/prime/prime.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common'; //Import necessary modules from NestJS
 
-@Controller('prime')
+@Controller('prime') // Set up the controller and its path
 export class PrimeController {
-    @Get(":primenum")
+    @Get(":primenum") // Make a GET endpoint that takes 'primenum'
     identifyNum(@Param('primenum') num:any ){
         if (num <= 1)
-            return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + false +"<br>}</p>";
+            return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + false +"<br>}</p>"; // Return false for numbers less than or equal to 1
 
         for (let i = 2; i <= Math.sqrt(num); i++)
         {
             if (num % i == 0)
-                return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + false +"<br>}</p>";
+                return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + false +"<br>}</p>"; // Return false if 'num' is divisible by any number other than 1 and itself
         }
 
-        return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + true +"<br>}</p>";
+        return "<p>{ <br> &nbsp;&nbsp;&nbsp;&nbsp;" + '"isPrime":' + true +"<br>}</p>"; // Return true if 'num' is not divisible by any number other than 1 and itself
     }
 }
